14242bed4cee3f16f80312145e8a5460ebddd206
WilliamTheBeginner <william78smart@hotmail.com>
2020-02-20 08:46:56 -0500

    reworded querySelector(#time), added new variabel time 2

--- app/js/main.ts
+++ app/js/main.ts
@@ -4,13 +4,14 @@ function getOrdinalNum(n: any) {
 
 var today = new Date();
 var time = today.getHours()  //time HH:MM
+var time2 = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
 var date = getOrdinalNum(today.getDate()) //date x + ordinal number (th)
 
 const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 document.querySelector('#date').innerHTML = monthNames[today.getMonth()] + ' ' + date
 
-document.querySelector('#time').innerHTML = today.getTime()
+document.querySelector('#time').innerHTML = time2 
 
 var hour = today.getHours()
 if (hour >= 08 && hour <= 11) {
