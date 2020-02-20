ed1482dab3219075f66531f6fcbeee9302a60ab7
WilliamTheBeginner <william78smart@hotmail.com>
2020-02-20 09:25:10 -0500

    Update main.js

--- app/js/dist/main.js
+++ app/js/dist/main.js
@@ -1,24 +1,32 @@
-function getOrdinalNum(n) {
+  
+function getOrdinalNum(n: any) {
     return n + (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : '');
 }
+
 var today = new Date();
-var time = today.getHours(); //time HH:MM
-var timeMinutes = today.getMinutes(); //minutes variable
+var time = today.getHours();  //time HH:MM
+var timeMinutes = today.getMinutes();
 timeMinutes = ( timeMinutes < 10 ? "0" : "" ) + timeMinutes;
+var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+var currentHours = today.getHours();
 
-var time2 = today.getHours() + ":" + timeMinutes;
+currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+currentHours = ( currentHours == 0 ) ? 12 : currentHours;
 
+var time2 = currentHours + ":" + timeMinutes;
 var date = getOrdinalNum(today.getDate()); //date x + ordinal number (th)
-var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 document.querySelector('#date').innerHTML = monthNames[today.getMonth()] + ' ' + date;
-document.querySelector('#time').innerHTML = time2;
+
+document.querySelector('#time').innerHTML = time2 + ' ' + timeofDay;
+
 var hour = today.getHours();
 if (hour >= 08 && hour <= 11) {
     document.querySelector('#greeting').innerHTML = 'Good Morning.';
-}
-else if (hour >= 12 && hour <= 17) {
+} else if (hour >= 12 && hour <= 17){
     document.querySelector('#greeting').innerHTML = 'Good Afternoon.';
-}
-else {
+} else {
     document.querySelector('#greeting').innerHTML = 'Good Evening.';
 }
