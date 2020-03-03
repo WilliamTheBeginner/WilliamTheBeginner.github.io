17dace6e1778c783485536a9fc20a8915e063fc5
WilliamTheBeginner <william78smart@hotmail.com>
2020-03-03 13:55:53 -0500

    new

---
+++ _site/app/js/main.ts
@@ -0,0 +1,35 @@
+
+function getOrdinalNum(n: any) {
+    return n + (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : '');
+}
+
+function updateTime(){
+    var today = new Date();
+    var time = today.getHours()  //time HH:MM
+    var timeOfDay = (time < 12) ? "AM" : "PM"
+    var time_hour = today.getHours()
+    time_hour = ( time_hour > 12 ) ? time_hour - 12 : time_hour
+    time_hour = ( time_hour == 0 ) ? 12 : time_hour
+
+    var time2 = time_hour + ":" + today.getMinutes() + " " + timeOfDay
+    var date = getOrdinalNum(today.getDate()) //date x + ordinal number (th)
+
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    document.querySelector('#date').innerHTML = monthNames[today.getMonth()] + ' ' + date
+
+    document.querySelector('#time').innerHTML = time2
+
+    var hour = today.getHours()
+    if (hour >= 05 && hour <= 11) {
+        document.querySelector('#greeting').innerHTML = 'Good Morning.'
+    } else if (hour >= 12 && hour <= 17){
+        document.querySelector('#greeting').innerHTML = 'Good Afternoon.'
+    } else {
+        document.querySelector('#greeting').innerHTML = 'Good Evening.'
+    }
+}
+
+setInterval(updateTime, 1000)
+
+//<body onload="updateTime()">
--- app/js/dist/main.js
+++ app/js/dist/main.js
@@ -1,32 +1,35 @@
-  
 function getOrdinalNum(n) {
     return n + (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : '');
 }
 
-var today = new Date();
-var time = today.getHours();  //time HH:MM
-var timeMinutes = today.getMinutes();
-timeMinutes = ( timeMinutes < 10 ? "0" : "" ) + timeMinutes;
-var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
-var currentHours = today.getHours();
+function updateTime(){
+    var today = new Date();
+    var time = today.getHours(); //time HH:MM
+    var timeMinutes = today.getMinutes(); //minutes variable
+    timeMinutes = ( timeMinutes < 10 ? "0" : "" ) + timeMinutes;
+    var timeOfDay = (time < 12) ? "AM" : "PM";
+    var time_hour = today.getHours();
+    time_hour = ( time_hour > 12 ) ? time_hour - 12 : time_hour;
+    time_hour = ( time_hour == 0 ) ? 12 : time_hour;
 
-currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+    var time2 = time_hour + ":" + timeMinutes + " " + timeOfDay;
 
-var time2 = currentHours + " : " + timeMinutes;
-var date = getOrdinalNum(today.getDate()); //date x + ordinal number (th)
-
-const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-document.querySelector('#date').innerHTML = monthNames[today.getMonth()] + ' ' + date;
-
-document.querySelector('#time').innerHTML = time2 + ' ' + timeofDay;
-
-var hour = today.getHours();
-if (hour >= 08 && hour <= 11) {
-    document.querySelector('#greeting').innerHTML = 'Good Morning.';
-} else if (hour >= 12 && hour <= 17){
-    document.querySelector('#greeting').innerHTML = 'Good Afternoon.';
-} else {
-    document.querySelector('#greeting').innerHTML = 'Good Evening.';
+    var date = getOrdinalNum(today.getDate()); //date x + ordinal number (th)
+    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    document.querySelector('#date').innerHTML = monthNames[today.getMonth()] + ' ' + date;
+    document.querySelector('#time').innerHTML = time2;
+    var hour = today.getHours();
+    if (hour >= 05 && hour <= 11) {
+        document.querySelector('#greeting').innerHTML = 'Good Morning.';
+    }
+    else if (hour >= 12 && hour <= 17) {
+        document.querySelector('#greeting').innerHTML = 'Good Afternoon.';
+    }
+    else {
+        document.querySelector('#greeting').innerHTML = 'Good Evening.';
+    }
 }
+
+setInterval(updateTime, 1000);
+
+//<body onload="updateTime()">
